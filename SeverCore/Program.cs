1c8fc38ca4b8eb6b32d729f6348bd667d8bbca4f
yozura <savedelove@naver.com>
2020-10-20 16:41:03 +0900

    1020_멀티 쓰레드 개론, 쓰레드 생성, 컴파일러 최적화

--- SeverCore/Program.cs
+++ SeverCore/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SeverCore
 {
     class Program
     {
+        volatile static bool _stop = false;
+
+        static void ThreadMain()
+        {
+            Console.WriteLine("쓰레드 시작");
+            
+            while(!_stop)
+            {
+                // 누군가가 stop 신호를 해주길 기다린다.
+            }
+            
+            Console.WriteLine("쓰레드 종료");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Task t = new Task(ThreadMain);
+            t.Start();
+
+            Thread.Sleep(1000);
+
+            _stop = true;
+
+            Console.WriteLine("Stop 호출");
+            Console.WriteLine("종료 대기중");
+            t.Wait();
+            Console.WriteLine("종료 성공");
         }
     }
 }
