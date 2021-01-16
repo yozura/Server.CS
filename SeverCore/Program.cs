8e79515b20722041163810dd4adbafca522cae0a
yozura <savedelove@naver.com>
2021-01-17 01:26:32 +0900

    0117_SpinLock 복습, Context Switching, AutoResetEvent, ManualResetEvent, Mutex, ReaderWriteLock
    오랜만에 다시 강의를 듣게 되서 전에 들었던 강의인 SpinLock 강의를 들으면서 마음을 다잡았고 Context Switching이 어떤 것인지 또 어떤 종류로 나뉘어서 활용하게 되는지 배웠습니다. AutoResetEvent와 ManualResetEvent, Mutex, ReaderWriteLock 등 설명하기 어려운부분은 따로 정보를 더 찾아봐야 할 것같습니다.

--- SeverCore/Program.cs
+++ SeverCore/Program.cs
@@ -1,73 +1,61 @@
 ﻿using System;
 using System.Threading;
-using System.Threading.Tasks;
+using System.Threading.Tasks;   
 
 namespace SeverCore
 {
-    class SpinLock
-    {
-        volatile int _locked = 0;
-
-        public void Acquire()
-        {
-            while(true)
-            {
-                //int original = Interlocked.Exchange(ref _locked, 1);
-                //if (original == 0)
-                //    break;
-
-                // CAS (Compare-And-Swap)
-                int expected = 0;
-                int desired = 1;
-                if(Interlocked.CompareExchange(ref _locked, desired, expected) == expected)
-                    break;
-            }
-
-
-        }
-
-        public void Release()
-        {
-            _locked = 0;
-        }
-    }
-
     class Program
     {
-        static int _num = 0;
-        static SpinLock _lock = new SpinLock();
+        // 1.근성
+        // 2.양보
+        // 3.갑질
 
-        static void Thread_1()
+            // Monitor
+        static object _lock = new object();
+        static SpinLock _lock2 = new SpinLock();
+        static ReaderWriterLockSlim _lock3 = new ReaderWriterLockSlim();
+        
+        class Reward
         {
-            for (int i = 0; i < 100000; i++)
-            {
-                _lock.Acquire();
-                _num++;
-                _lock.Release();
-            }
+
         }
 
-        static void Thread_2()
+        static Reward GetRewardById(int id)
         {
-            for (int i = 0; i < 100000; i++)
+            _lock3.EnterReadLock();
+            _lock3.ExitReadLock();
+            lock(_lock)
             {
-                _lock.Acquire();
-                _num--;
-                _lock.Release();
+
             }
+            return null;
+        }
+
+        static void AddReward(Reward reward)
+        {
+            _lock3.EnterWriteLock();
+            _lock3.ExitWriteLock();
         }
 
         static void Main(string[] args)
         {
-            Task t1 = new Task(Thread_1);
-            Task t2 = new Task(Thread_2);
+            // object
+            lock(_lock)
+            {
 
-            t1.Start();
-            t2.Start();
+            }
 
-            Task.WaitAll(t1, t2);
-
-            Console.WriteLine(_num);
+            bool lockTaken = false;
+            try
+            {
+                _lock2.Enter(ref lockTaken);
+            }
+            finally
+            {
+                if(lockTaken)
+                    _lock2.Exit();
+            }
         }
+
     }
 }
