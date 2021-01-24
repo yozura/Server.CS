17e6be7f1a6ca5d57a9499f529a6f2208404256f
yozura <savedelove@naver.com>
2021-01-24 22:07:29 +0900

    0124_UTF-8,16 패킷 직렬화 3~4
    문자열을 패킷으로 직렬화 시킬 때 어떤 유형을 선택해서 할 것인지 배웠습니다. 기본적으로 C# 에서의 문자열은 UTF-16로 작성되기 때문에 Encoding 라이브러리를 사용해 Unicode 클래스를 이용했습니다. UTF-8은 영문에 1byte를 부여하고 한글에 3byte를 할당합니다. UTF-16은 영문과 한글에 또는 BMP에 포함되어있는 문자들에 한해서 2byte 씩 할당합니다. 예외적으로 BMP 이후 문자에는 4byte 씩 할당합니다. 또 List를 패킷으로 직렬화 시키는 방법도 배웠습니다.

--- Server/ClientSession.cs
+++ Server/ClientSession.cs
@@ -1,6 +1,8 @@
 ﻿using ServerCore;
 using System;
+using System.Collections.Generic;
 using System.Net;
+using System.Text;
 using System.Threading;
 
 namespace Server
@@ -11,25 +13,79 @@ namespace Server
         public ushort packetId;
 
         public abstract ArraySegment<byte> Write();
-        public abstract void Read(ArraySegment<byte> array);
+        public abstract void Read(ArraySegment<byte> segment);
     }
 
     class PlayerInfoReq : Packet
     {
         public long playerId;
+        public string name;
+
+        public struct SkillInfo
+        {
+            public int id;
+            public short level;
+            public float duration;
+
+            public bool Write(Span<byte> s, ref ushort count)
+            {
+                bool success = true;
+                success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), id);
+                count += sizeof(int);
+                success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), level);
+                count += sizeof(short);
+                success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), duration);
+                count += sizeof(float);
+
+                return success;
+            }
+
+            public void Read(ReadOnlySpan<byte> s, ref ushort count)
+            {
+                id = BitConverter.ToInt32(s.Slice(count, s.Length - count));
+                count += sizeof(int);
+                level = BitConverter.ToInt16(s.Slice(count, s.Length - count));
+                count += sizeof(short);
+                duration = BitConverter.ToSingle(s.Slice(count, s.Length - count));
+                count += sizeof(float);
+            }
+        }
+
+        public List<SkillInfo> skills = new List<SkillInfo>();
 
         public PlayerInfoReq()
         {
             this.packetId = (ushort)PacketID.PlayerInfoReq;
         }
 
-        public override void Read(ArraySegment<byte> array)
+        public override void Read(ArraySegment<byte> segment)
         {
             ushort count = 0;
-            count += 2;
-            count += 2;
-            this.playerId = BitConverter.ToInt64(new ReadOnlySpan<byte>(array.Array, array.Offset + count, array.Count - count));
-            count += 8;
+
+            ReadOnlySpan<byte> s = new ReadOnlySpan<byte>(segment.Array, segment.Offset, segment.Count);
+
+            count += sizeof(ushort);
+            count += sizeof(ushort);
+            this.playerId = BitConverter.ToInt64(s.Slice(count, s.Length - count));
+            count += sizeof(long);
+
+            // string
+            ushort nameLen = BitConverter.ToUInt16(s.Slice(count, s.Length - count));
+            count += sizeof(ushort);
+            this.name = Encoding.Unicode.GetString(s.Slice(count, nameLen));
+            count += nameLen;
+
+            // Skill List
+            ushort skillLen = BitConverter.ToUInt16(s.Slice(count, s.Length - count));
+            count += sizeof(ushort);
+
+            skills.Clear();
+            for (int i = 0; i < skillLen; i++)
+            {
+                SkillInfo skill = new SkillInfo();
+                skill.Read(s, ref count);
+                skills.Add(skill);
+            }
         }
 
         public override ArraySegment<byte> Write()
@@ -38,12 +94,29 @@ namespace Server
             ushort count = 0;
             bool success = true;
 
-            count += 2;
-            success &= BitConverter.TryWriteBytes(new Span<byte>(openSegment.Array, openSegment.Offset + count, openSegment.Count - count), this.packetId);
-            count += 2;
-            success &= BitConverter.TryWriteBytes(new Span<byte>(openSegment.Array, openSegment.Offset, openSegment.Count - count), this.playerId);
-            count += 8;
-            success &= BitConverter.TryWriteBytes(new Span<byte>(openSegment.Array, openSegment.Offset, openSegment.Count), count);
+            Span<byte> s = new Span<byte>(openSegment.Array, openSegment.Offset, openSegment.Count);
+
+            count += sizeof(ushort);
+            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), this.packetId);
+            count += sizeof(ushort);
+            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), this.playerId);
+            count += sizeof(long);
+
+            // string
+            ushort nameLen = (ushort)Encoding.Unicode.GetBytes(this.name, 0, this.name.Length, openSegment.Array, openSegment.Offset + count + sizeof(ushort));
+            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), nameLen);
+            count += sizeof(ushort);
+            count += nameLen;
+
+            // Skill List
+            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), (ushort)skills.Count);
+            count += sizeof(ushort);
+
+            foreach (SkillInfo skill in skills)
+                success &= skill.Write(s, ref count);
+
+            // Size
+            success &= BitConverter.TryWriteBytes(s, count);
 
             if (success == false)
                 return null;
@@ -93,7 +166,10 @@ namespace Server
                     {
                         PlayerInfoReq p = new PlayerInfoReq();
                         p.Read(buffer);
-                        Console.WriteLine($"PlayerInfoReq : {p.playerId}");
+                        Console.WriteLine($"PlayerInfoReq : {p.playerId} : {p.name}");
+
+                        foreach(PlayerInfoReq.SkillInfo skill in p.skills)
+                            Console.WriteLine($"Skill({skill.id})({skill.level})({skill.duration})");
                     }
                     break;
             }
